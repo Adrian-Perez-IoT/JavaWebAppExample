6f6649dc058616257be8cb6fd7d2b3eb3e4ac048
Adrián Pérez <perezjadrian@gmail.com>
2021-03-12 13:00:58 -0300

    commit init

---
+++ src/main/java/zone/webtraining/museums_finder/HelloApplication.java
@@ -0,0 +1,9 @@
+package zone.webtraining.museums_finder;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
